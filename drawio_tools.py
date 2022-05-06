a8a5e2f964ace58dfaa6b20232f4b7df56fbc2f1
GanizaniSitara <dcx45@hotmail.com>
2022-05-06 13:29:32 +0100

    first commit

---
+++ drawio_tools.py
@@ -0,0 +1,56 @@
+import base64, zlib
+from urllib.parse import quote, unquote, quote_plus
+
+# functions courtesy of
+# https://stackoverflow.com/questions/46351275/using-pako-deflate-with-python
+# TODO: refactor
+def js_encode_uri_component(data):
+    return quote(data, safe='~()*!.\'')
+
+def js_decode_uri_component(data):
+    return unquote(data)
+
+def js_string_to_byte(data):
+    return bytes(data, 'iso-8859-1')
+
+def js_bytes_to_string(data):
+    return data.decode('iso-8859-1')
+
+def js_btoa(data):
+    return base64.b64encode(data)
+
+def js_atob(data):
+    return base64.b64decode(data)
+
+def pako_deflate_raw(data):
+    compress = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -15, memLevel=8,strategy=zlib.Z_DEFAULT_STRATEGY)
+    compressed_data = compress.compress(data)
+    compressed_data += compress.flush()
+    return compressed_data
+
+def pako_inflate_raw(data):
+    decompress = zlib.decompressobj(-15)
+    decompressed_data = decompress.decompress(data)
+    decompressed_data += decompress.flush()
+    return decompressed_data
+
+def decode_diagram_data(data):
+    data = js_atob(data)
+    data = pako_inflate_raw(data)
+    data = data.decode()
+    data = unquote(data)
+    return data
+
+def encode_diagram_data(data):
+    # https://stackoverflow.com/questions/33547976/using-python-quote-plus-with-slashes
+    data = quote(data, safe='~()*!.\'')
+    data = data.encode()
+    data = pako_deflate_raw(data)
+    data = js_btoa(data)
+    return data
+
+
+
+
+
+
